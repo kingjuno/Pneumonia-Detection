884388f3734d2b0dae9a405abaf4597864b02d80
Geo Jolly <geojollyc@gmail.com>
2022-01-12 11:56:16 +0530

    Enhance the model

--- app.py
+++ app.py
@@ -3,14 +3,20 @@ import numpy as np
 from preprocess import preprocess
 from predict import predict
 
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
 def classify(filepath):
     input_batch = preprocess(filepath)
-    result = np.argmax(predict(input_batch))
-    return 'Pneumonia' if result == 1 else 'Normal'
-
+    result = predict(input_batch)
+    result = softmax(result).ravel()
+    result_ind = np.argmax(result)
+    confidence_score = round(result[result_ind]*100,10)
+    return f'Pneumonia {confidence_score}%' if result_ind == 1 else f'Normal {confidence_score}%' 
 
 title = "Pnuemonia Detection from chest X-Ray using PyTorch"
-description = "Detecting Pnuemonia using chest X-Ray "
+description = "Detecting Pnuemonia using chest X-Ray ||"
 article = """<p style='text-align: center'>
         <a href='https://nbviewer.org/github/kingjuno/Pneumonia-Detection/blob/master/notebook/pneumonia-det.ipynb?flush_cache=true' target='_blank'>Notebook Link</a>
         <br><a href='https://github.com/kingjuno/Pneumonia-Detection' target='_blank'>Github Repo</a></p>
@@ -27,4 +33,4 @@ gr.Interface(classify,
             outputs= "text", title=title,
             description=description,
             article=article,
-            examples=examples).launch(debug=False, enable_queue=True)
+            examples=examples).launch(debug=True, enable_queue=True)
